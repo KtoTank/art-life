fa71a38fc963ff9308b1c0b8c40bb87df382550f
KtoTank <you@example.com>
2024-05-02 19:03:08 +0600

    Добавил компоненты "Main", "Header". Подключил TailwindCss, добавил в конфиг катсомные цвета.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+       transparent: 'transparent',
+      current: 'currentColor',
+      white: '#fff',
+      cream: '#DDD5D0',
+      accentBlue: '#2C3D55',
+      accentFio: '#6A687A',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
